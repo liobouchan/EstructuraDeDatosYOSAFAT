f28d076050ff771887061aaceaf29e3af69e7b81
liobouchan <leo.bouchan@gmail.com>
2014-08-20 20:47:23 -0500

    Code in function

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#define TAM 50
+
+typedef struct Tienda{
+  char prod[TAM];
+  float precio;
+  int SKU;
+}tienda;
+
+void escribe(tienda *);
+void lee(tienda *);
+
+void main(void){
+  /* code */
+  tienda p01, p02, p03;
+  lee(&p01);
+  lee(&p02);
+  escribe(&p01);
+  escribe(&p02);
+  p03 = (tienda *)malloc(sizeof(tienda));
+}
+
+void escribe(tienda *ap){
+
+}
+void lee(tienda *ap){
+  char c;
+  printf("Introduce SKU: ");
+  scanf("%d", &(*ap).SKU);
+  printf("Introduce nombre de producto:");
+  gets(ap->prod);
+}
